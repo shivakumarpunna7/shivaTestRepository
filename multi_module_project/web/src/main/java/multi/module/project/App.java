e13c07da12e3b7ea9a0458ecec1d7dcfd4950567
shivakumarpunna7 <shivakumarpunna777@gmail.com>
2017-05-15 15:34:49 +0530

    Spring maven multi module setup

---
+++ multi_module_project/web/src/main/java/multi/module/project/App.java
@@ -0,0 +1,13 @@
+package multi.module.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
